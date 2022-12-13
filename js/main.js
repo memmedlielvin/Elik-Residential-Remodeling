e024de861abeed9860f09e4d1b3a475fb7cdb31b
memmedli elvin <memmedlielvindevop@gmail.com>
2022-12-13 16:43:38 +0400

    finish

--- js/main.js
+++ js/main.js
@@ -12,10 +12,34 @@ function openChat() {
   document.querySelector("main").style.display = "none";
   document.querySelector("#chat-box").style.display = "block";
   document.querySelector("#lets-chat").style.visibility = "hidden";
+  document.querySelector("#arrow-up").style.display = "none";
 }
 
 function closeChat() {
   document.querySelector("main").style.display = "block";
   document.querySelector("#chat-box").style.display = "none";
   document.querySelector("#lets-chat").style.visibility = "visible";
+  document.querySelector("#arrow-up").style.display = "block";
+}
+
+function inputFocus() {
+  document.querySelector("#send-message-icon").style.backgroundColor =
+    "#6C655C";
+  document.querySelector("#send-message-icon").style.color = "white";
+  document.querySelector("#send-message-icon").style.padding = "3px";
+  document.querySelector("#send-message-icon").style.borderRadius = "2px";
+}
+
+function inputBlur() {
+  document.querySelector("#send-message-icon").style.backgroundColor = "white";
+  document.querySelector("#send-message-icon").style.color = "black";
+  document.querySelector("#send-message-icon").style.padding = "0";
+  document.querySelector("#send-message-icon").style.borderRadius = "0";
+}
+
+function sendMessage() {
+  var inputVal = document.querySelector("#input-text").value;
+  document.querySelector("#chat-box-center-inner").innerHTML += inputVal;
+  document.querySelector("#chat-box-center-inner").style.backgroundColor =
+    "#6C655C";
 }
